a697d4a6d253bc02827e92fe01cbd581dc5d2bd6
hongboliu <hongboliu@tencent.com>
2019-10-21 17:37:59 +0800

    second

--- a.js
+++ a.js
@@ -1,1 +1,1 @@
-window.onload=()=>{b='';d=document;$=d.getElementsByTagName.bind(d);for(i=0;i<7;i++){b+=$('span')[i].innerText}$('input')[0].value=eval(b);$('button')[0].click()}
+function f(){b='';d=document;$=d.getElementsByTagName.bind(d);for(i=0;i<7;i++){b+=$('span')[i].innerText}$('input')[0].value=eval(b);$('button')[0].click()}
